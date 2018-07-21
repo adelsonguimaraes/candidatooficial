979851615445929254c4395f7442bec8a1ae901e
adelsonguimaraes <adelsonguimaraes@gmail.com>
2018-07-21 01:23:10 -0400

    Init

---
+++ app.js
@@ -0,0 +1,10 @@
+(function () {
+    angular.module('jajaooficial', [
+        'ui.router',                    // Routing
+        'oc.lazyLoad',                  // ocLazyLoad
+        'ui.bootstrap',                 // Ui Bootstrap
+        'pascalprecht.translate',       // Angular Translate
+        'ngIdle',                       // Idle timer
+        'ngSanitize'                    // ngSanitize
+    ]);
+});
